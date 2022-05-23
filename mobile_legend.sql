230efb840017a9a3510185c3ebd54f8aea25b308
Agung Gumelar <86769833+hellogung@users.noreply.github.com>
2022-05-23 07:43:01 -0400

    add db.sql

---
+++ mobile_legend.sql
@@ -0,0 +1,93 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: May 23, 2022 at 06:36 PM
+-- Server version: 5.7.34
+-- PHP Version: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mobile_legend`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gear`
+--
+
+CREATE TABLE `gear` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `hero_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hero`
+--
+
+CREATE TABLE `hero` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `skill` varchar(255) NOT NULL,
+  `story` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `hero`
+--
+
+INSERT INTO `hero` (`id`, `name`, `role`, `skill`, `story`) VALUES
+(1, 'Alucard', 'Fighter', 'Flicker', 'Lorem ipsum dolor sit amet consectetur adipisicing Dolorum, praesentium provident enim fugiat assumenda laboriosam, inventore ipsam beatae quis fuga facere hic nihil quisquam consequuntur quaerat, ipsum repellendus numquam. Quam molestias maxime suscipit veritatis, deserunt beatae maiores velit, quasi sunt impedit quos quidem illo corrupti pariatur. Ipsum magnam deserunt qui magni officiis, dolor ex eveniet tempora hic et est qual fugiat inventore doloribus repellendus obcaecati nam nobis ipsa. Laboriosam, non iste magni\r\n');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `gear`
+--
+ALTER TABLE `gear`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `hero`
+--
+ALTER TABLE `hero`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `gear`
+--
+ALTER TABLE `gear`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `hero`
+--
+ALTER TABLE `hero`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
